39bdb305f3856fd94a186ccbc17d169512ce69c1
Ryan Djemtchemeu <ryanmambou@gmail.com>
2024-09-23 17:37:13 +0100

    first commit

---
+++ src/config/typeorm.ts
@@ -0,0 +1,24 @@
+import { DataSource } from 'typeorm';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'dotenv';
+import { join } from 'path';
+config();
+
+const configService = new ConfigService();
+
+console.log('Pathname: ', __dirname);
+
+const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: configService.get<string>('DATABASE_HOST'),
+  port: parseInt(configService.get<string>('DATABASE_PORT'), 5432),
+  username: configService.get<string>('DATABASE_USER'),
+  password: configService.get<string>('DATABASE_PASSWORD'),
+  database: configService.get<string>('DATABASE_NAME'),
+  synchronize: false,
+  entities: [__dirname + '/core/**/*.entity{.ts,.js}'],
+  // entities: ['src/database/core/**/*.entity{.ts,.js}'],
+  migrations: ['src/database/migrations/*-migration.ts'],
+});
+
+export default AppDataSource;
